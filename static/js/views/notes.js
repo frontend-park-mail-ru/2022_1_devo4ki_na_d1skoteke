f6c8e16e13f81fb21a3a5503e651697f3680d3c6
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-03-03 19:25:07 +0300

    added dynamicly loaded notes page

--- static/js/views/notes.js
+++ static/js/views/notes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { render } from "./note";
+
 const leftPanel = () => {
     const leftPanel = document.createElement('div');
     leftPanel.classList.add('sidebar');
@@ -17,14 +19,6 @@ const notesTopPanel = () => {
 export const Notes = () => {
     window.history.pushState(null, null, "/notes");
     const root = document.getElementById('root');
-    root.innerHTML = '';
 
-    root.appendChild(leftPanel());
-
-    const notes = document.createElement('div');
-    notes.classList.add('notes');
-    notes.appendChild(notesTopPanel());
-
-    root.append(notes);
-
+    render(root);
 }
