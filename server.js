0cbda98066391fccae9259b3d40339fa6e4eb6ad
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-03-06 22:25:18 +0300

    deleted unused

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const path = require('path');
 
-const routes = require('./routes/routes')
-
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.use("/", routes);
 
 const listener = app.listen(3000, () => {
     console.log("App is listening on port " + listener.address().port);
