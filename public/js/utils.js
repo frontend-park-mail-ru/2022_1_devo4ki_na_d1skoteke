0addd3ec2b458b793d1b131c3037dbc1250685b8
erik770 <mr.erik770@mail.ru>
2022-03-07 11:47:57 +0300

    TP-070 delete unused code from utils

--- public/js/utils.js
+++ public/js/utils.js
@@ -15,13 +15,6 @@ export const SetFavicon = () => {
   document.querySelector('head').appendChild(addFavicon);
 };
 
-export const createLogo = () => {
-  const logo = document.createElement('img');
-  logo.classList.add('logo');
-  logo.src = './img/signup_login_logo.png';
-  return logo;
-};
-
 export const validateEmail = (email) => {
   if (email === '') {
     return InvalidStatusType.EMPTY;
@@ -34,32 +27,6 @@ export const validateEmail = (email) => {
   // TODO: check email in db and return ALREADY_EXISTS status
 
   return InvalidStatusType.VALID;
-};
-
-export const createInput = (type, text, name) => {
-  const input = document.createElement('input');
-  input.type = type;
-  input.name = name;
-  input.placeholder = text;
-
-  return input;
-};
-
-export const createLabel = (text) => {
-  const label = document.createElement('label');
-  label.classList.add('my-label');
-  label.innerHTML = text;
-  return label;
-};
-
-export const createBadEmailTextError = () => {
-  const badEmailText = document.createElement('div');
-  badEmailText.innerHTML = 'Please enter valid email';
-  badEmailText.classList.add('bad-input');
-  badEmailText.id = 'bad-email';
-  badEmailText.style.display = 'none';
-
-  return badEmailText;
 };
 
 export const addValidationEmail = () => {
@@ -88,37 +55,3 @@ export const addValidationEmail = () => {
   });
 };
 
-// export const createEmailInput = () => {
-//   const emailForm = document.createElement('div');
-//   emailForm.classList.add('form');
-//
-//   const emailInput = createInput('email', 'Enter email', 'email');
-//   emailInput.id = 'email-input';
-//
-//   emailForm.appendChild(createLabel('Email'));
-//   emailForm.appendChild(emailInput);
-//   emailForm.appendChild(createBadEmailTextError());
-//
-//   emailInput.addEventListener('focusout', (e) => {
-//     switch (validateEmail(e.target.value)) {
-//       case InvalidStatusType.WRONG_SYMBOLS:
-//         emailForm.querySelector('#bad-email').style.display = 'block';
-//         emailForm.querySelector('#bad-email')
-//           .innerHTML = 'Please enter valid email';
-//         break;
-//       // TODO: ALREADY_EXISTS status handler
-//       case InvalidStatusType.EMPTY:
-//         emailForm.querySelector('#bad-email').style.display = 'none';
-//         break;
-//       case InvalidStatusType.VALID:
-//         emailForm.querySelector('#bad-email').style.display = 'none';
-//         break;
-//       default:
-//     }
-//   });
-//   emailInput.addEventListener('input', () => {
-//     emailForm.querySelector('#bad-email').style.display = 'none';
-//   });
-//
-//   return emailForm;
-// };
