8e148041a102ea27cdd976eb4fdee86e356ce929
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-03-08 16:54:51 +0300

    added mock for server

--- server.js
+++ server.js
@@ -26,71 +26,45 @@ const compileTemplate = () => {
 
 compileTemplate();
 
-app.use('/note', (req, res) => {
-  const bookStore = [
-    {
-      title: 'Templating with Pug',
-      author: 'Winston Smith',
-      pages: 143,
-      year: 2017,
-    },
-    {
-      title: 'Node.js will help',
-      author: 'Guy Fake',
-      pages: 879,
-      year: 2015,
-    },
-  ];
-  res.render('sidebar', { name: 'Jhon', title: 'Lectures for technopark', bookStore });
-});
-
-app.use('/body', (req, res) => {
-  const bookStore = [
-    {
-      title: 'Templating with Pug',
-      author: 'Winston Smith',
-      pages: 143,
-      year: 2017,
-    },
-    {
-      title: 'Node.js will help',
-      author: 'Guy Fake',
-      pages: 879,
-      year: 2015,
-    },
-  ];
-  res.render('sidebar2', { name: 'Jhon', title: 'Lectures for technopark', bookStore });
-});
-
-app.use('/books', (req, res) => {
-  const bookStore = [
-    {
-      title: 'Templating with Pug',
-      author: 'Winston Smith',
-      pages: 143,
-      year: 2017,
-    },
-    {
-      title: 'Node.js will help',
-      author: 'Guy Fake',
-      pages: 879,
-      year: 2015,
-    },
-  ];
-
-  res.render('books', {
-    bookStore,
-  });
-});
-
-app.use('/yyy', (req, res) => {
-  res.render('yyy', { title: 'ffff' });
-});
-
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+
+
+app.use('/api/v1/note/:id', (req, res) => {
+  const id = req.params.id;
+  res.json({name: `somerandomshitname${id}`, body: `somarandombody${id}`});
+})
+
+app.use('/api/v1/notes', (req, res) => {
+
+
+  const body = {
+    notes: [
+      {
+        name: "hehe 1 note",
+        favincon: "s3/erfgefwfwef",
+        token: "API-ion-2342341242regf3qf3f3f3wfd"
+      },
+      {
+        name: "hehe 2 note",
+        favincon: "s3/erfgefwfwef",
+        token: "API-ion-2342341242regf3qf3f3f3wfd"
+      },
+      {
+        name: "hehe 3 note",
+        favincon: "s3/erfgefwfwef",
+        token: "API-ion-2342341242regf3qf3f3f3wfd"
+      }
+    
+    ]
+    }
+
+  res.json(body);
+})
+
 const listener = app.listen(3000, () => {
   console.log(`App is listening on port ${listener.address().port}`);
 });
+
