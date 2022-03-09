acbd854a83437faab808708c3b62ce076e45d12d
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-03-09 13:05:16 +0300

    added linter to view

--- public/views/note.js
+++ public/views/note.js
@@ -1,22 +1,22 @@
-import { CreateLeftSide } from '../components/LeftSideBar/LeftSideBar.js';
-import { CreateNoteContent } from '../components/NoteContent/NoteContent.js';
-import { ApiStore } from '../store/ApiStore.js';
+import { CreateLeftSide } from "../components/LeftSideBar/LeftSideBar.js";
+import { CreateNoteContent } from "../components/NoteContent/NoteContent.js";
+import { ApiStore } from "../store/ApiStore.js";
 
 export const note = async (node) => {
-  const page = document.createElement('div');
+  const page = document.createElement("div");
 
-  page.classList.add('notion__whole__page');
+  page.classList.add("notion__whole__page");
 
   const back = new ApiStore();
 
   const fetchRes = await back.GetAllNotes();
   const bookStore = fetchRes.notes;
 
-  CreateLeftSide(page, { name: 'Henry', bookStore });
+  CreateLeftSide(page, { name: "Henry", bookStore });
 
-  const tno = await back.GetNoteByToken('My first note');
+  const tno = await back.GetNoteByToken("My first note");
 
-  CreateNoteContent(page, { name: 'Henry', title: tno.name, bookStore });
+  CreateNoteContent(page, { name: "Henry", title: tno.name, bookStore });
 
   node.appendChild(page);
 };
