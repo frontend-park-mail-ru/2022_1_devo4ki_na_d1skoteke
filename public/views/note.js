22feb718eddfd572dd1e80a27aa51f6588bc951e
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-03-26 21:11:08 +0300

    added dynamic reload for note on click

--- public/views/note.js
+++ public/views/note.js
@@ -2,9 +2,25 @@ import { CreateLeftSide } from '../components/LeftSideBar/LeftSideBar.js';
 import { CreateNoteContent } from '../components/NoteContent/NoteContent.js';
 import { ApiStore } from '../store/ApiStore.js';
 
+
+export const render = async (id, userNotes) => {
+  let body = document.getElementById("body");
+  let title = document.getElementById("title");
+
+  const book = userNotes.forEach(element => {
+    if (element.name = id) {
+      return element;
+    }
+  });
+
+  body.innerText = book.name;
+  title.innerText = book.body;
+  
+}
+
 /* eslint no-param-reassign: ["error", { "props": false }] */
 export const note = async (node) => {
-  
+
   node.innerHTML = '';
 
   const page = document.createElement('div');
@@ -16,8 +32,9 @@ export const note = async (node) => {
   const bookStore = fetchRes.notes;
 
   CreateLeftSide(page, { name: 'Henry', bookStore });
+  const book = bookStore[0];
 
-  CreateNoteContent(page, { name: 'Henry', title: '1', bookStore });
+  CreateNoteContent(page, { book });
 
   node.appendChild(page);
 
