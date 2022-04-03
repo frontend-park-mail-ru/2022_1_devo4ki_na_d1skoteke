c371e54a4917a8667222a24442b9d26271ee27f1
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-04-03 18:56:41 +0300

    TP-f1f: added styles for logout buttin

--- public/components/NoteContent/NoteContent.js
+++ public/components/NoteContent/NoteContent.js
@@ -2,8 +2,9 @@ import { NoteContent } from './compiled/NoteContent.js';
 
 export const CreateNoteContent = (node, context) => {
   const LeftSide = document.createElement('div');
-
   LeftSide.classList.add('notion__content');
   LeftSide.innerHTML = NoteContent(context);
   node.appendChild(LeftSide);
+  const logout = document.getElementById('logout');
+  logout.dataset.section = 'logout';
 };
